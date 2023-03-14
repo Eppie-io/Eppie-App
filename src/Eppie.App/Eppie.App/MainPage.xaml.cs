dfc6864aededad46f0fe9acd43eb5173ef565887
alexander <116556921+al-kau@users.noreply.github.com>
2023-03-14 19:53:33 +0200

    add license header (#3)

--- src/Eppie.App/Eppie.App/MainPage.xaml.cs
+++ src/Eppie.App/Eppie.App/MainPage.xaml.cs
@@ -1,4 +1,22 @@
-﻿using Microsoft.UI.Xaml;
+﻿///////////////////////////////////////////////////////////////////////////////
+//
+//   Copyright 2023 Eppie(https://eppie.io)
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
@@ -12,8 +30,6 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace Eppie.App
 {
