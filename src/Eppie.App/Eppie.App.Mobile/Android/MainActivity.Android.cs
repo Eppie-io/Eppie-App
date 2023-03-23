8b9f4600a2450345fa8bb229e0ee1e6fdb862dce
alexander <116556921+al-kau@users.noreply.github.com>
2023-03-23 22:23:52 +0200

    The new uno template (v4.8) (#28)
    * remove solution
    
    * add uno project (.net7; android, ios, macos, wasm, win, gtk; mvvm; ui tests; dependency injecton: config, localization, default-logging; fluent theme)

--- src/Eppie.App/Eppie.App.Mobile/Android/MainActivity.Android.cs
+++ src/Eppie.App/Eppie.App.Mobile/Android/MainActivity.Android.cs
@@ -1,33 +1,17 @@
-// ---------------------------------------------------------------------------- //
-//                                                                              //
-//   Copyright 2023 Eppie(https://eppie.io)                                     //
-//                                                                              //
-//   Licensed under the Apache License, Version 2.0 (the "License"),            //
-//   you may not use this file except in compliance with the License.           //
-//   You may obtain a copy of the License at                                    //
-//                                                                              //
-//       http://www.apache.org/licenses/LICENSE-2.0                             //
-//                                                                              //
-//   Unless required by applicable law or agreed to in writing, software        //
-//   distributed under the License is distributed on an "AS IS" BASIS,          //
-//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   //
-//   See the License for the specific language governing permissions and        //
-//   limitations under the License.                                             //
-//                                                                              //
-// ---------------------------------------------------------------------------- //
-
 using Android.App;
+using Android.Content.PM;
+using Android.OS;
 using Android.Views;
+using Android.Widget;
 
-namespace Eppie.App
+namespace Eppie.App.Droid
 {
     [Activity(
-            MainLauncher = true,
-            ConfigurationChanges = global::Uno.UI.ActivityHelper.AllConfigChanges,
-            WindowSoftInputMode = SoftInput.AdjustPan | SoftInput.StateHidden
-        )]
+        MainLauncher = true,
+        ConfigurationChanges = global::Uno.UI.ActivityHelper.AllConfigChanges,
+        WindowSoftInputMode = SoftInput.AdjustNothing | SoftInput.StateHidden
+    )]
     public class MainActivity : Microsoft.UI.Xaml.ApplicationActivity
     {
     }
 }
-
---
+++ src/Eppie.App/Eppie.App/Business/Models/AppConfig.cs
@@ -0,0 +1,8 @@
+
+namespace Eppie.App.Business.Models
+{
+    public record AppConfig
+    {
+        public string? Title { get; init; }
+    }
+}
