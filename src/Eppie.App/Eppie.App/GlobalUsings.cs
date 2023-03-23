fe7b54390817ff4402e902e6f64849e4d3f33c97
alexander <kauhan+github@eppie.io>
2023-03-23 16:55:05 +0200

    clear up .cs files

--- src/Eppie.App/Eppie.App/GlobalUsings.cs
+++ src/Eppie.App/Eppie.App/GlobalUsings.cs
@@ -1,25 +1,28 @@
-﻿global using Eppie.App.Business.Models;
-global using Microsoft.Extensions.DependencyInjection;
+﻿// ---------------------------------------------------------------------------- //
+//                                                                              //
+//   Copyright 2023 Eppie(https://eppie.io)                                     //
+//                                                                              //
+//   Licensed under the Apache License, Version 2.0 (the "License"),            //
+//   you may not use this file except in compliance with the License.           //
+//   You may obtain a copy of the License at                                    //
+//                                                                              //
+//       http://www.apache.org/licenses/LICENSE-2.0                             //
+//                                                                              //
+//   Unless required by applicable law or agreed to in writing, software        //
+//   distributed under the License is distributed on an "AS IS" BASIS,          //
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   //
+//   See the License for the specific language governing permissions and        //
+//   limitations under the License.                                             //
+//                                                                              //
+// ---------------------------------------------------------------------------- //
+
+global using Eppie.App.Business.Models;
 global using Microsoft.Extensions.Hosting;
-global using Microsoft.Extensions.Localization;
 global using Microsoft.Extensions.Logging;
-global using Microsoft.Extensions.Options;
 global using Microsoft.UI.Xaml;
 global using Microsoft.UI.Xaml.Controls;
-global using Microsoft.UI.Xaml.Media;
-global using Microsoft.UI.Xaml.Navigation;
-global using System;
-global using System.Collections.Generic;
-global using System.Collections.Immutable;
-global using System.Linq;
-global using System.Net.Http;
-global using System.Threading;
-global using System.Threading.Tasks;
 global using Uno.Extensions;
 global using Uno.Extensions.Configuration;
 global using Uno.Extensions.Hosting;
 global using Uno.Extensions.Localization;
-global using Uno.Extensions.Logging;
-global using Windows.ApplicationModel;
 global using Application = Microsoft.UI.Xaml.Application;
-global using ApplicationExecutionState = Windows.ApplicationModel.Activation.ApplicationExecutionState;
