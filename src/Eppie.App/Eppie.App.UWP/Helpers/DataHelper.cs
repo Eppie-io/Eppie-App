e50798518ab9633ccdceed8e05383a392991d94b
Valera <>
2024-05-23 18:15:58 +0300

    UWP added

---
+++ src/Eppie.App/Eppie.App.UWP/Helpers/DataHelper.cs
@@ -0,0 +1,13 @@
+﻿using Windows.Storage;
+
+
+namespace Tuvi.App.Helpers
+{
+    public static class DataHelper
+    {
+        public static string GetAppDataPath()
+        {
+            return ApplicationData.Current.LocalFolder.Path;
+        }
+    }
+}
