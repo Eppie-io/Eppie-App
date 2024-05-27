1570736a9162a73fdd88e76dbe602e7a0bf81e7d
Valera <50830352+ValeraFinebits@users.noreply.github.com>
2024-05-27 14:11:56 +0300

    Uwp added (#254)
    * UWP added
    
    
    ---------
    
    Co-authored-by: Valera <>

---
+++ src/Eppie.App/Tuvi.OAuth2/TokenRefresher.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Finebits.Authorization.OAuth2.Abstractions;
+using Finebits.Authorization.OAuth2.Types;
+using Tuvi.Core;
+using Tuvi.Core.Entities;
+
+namespace Tuvi.OAuth2
+{
+    public class TokenRefresher : ITokenRefresher
+    {
+        public static ITokenRefresher CreateTokenRefresher(AuthorizationProvider authorizationProvider)
+        {
+            return new TokenRefresher(authorizationProvider);
+        }
+
+        private static string EmptyOrWhiteSpaceExceptionString { get => "The value cannot be an empty string or composed entirely of whitespace."; }
+        private static readonly Func<string, string> EmailServiceNotSupportedExceptionString = (email) => $"'{email}' email service is not supported.";
+
+        private readonly AuthorizationProvider _authorizationProvider;
+
+        private TokenRefresher(AuthorizationProvider authorizationProvider)
+        {
+            _authorizationProvider = authorizationProvider;
+        }
+
+        public async Task<AuthToken> RefreshTokenAsync(string mailServiceName, string refreshToken, CancellationToken cancellationToken = default)
+        {
+            if (string.IsNullOrWhiteSpace(mailServiceName))
+            {
+                throw new ArgumentException(EmptyOrWhiteSpaceExceptionString, nameof(mailServiceName));
+            }
+
+            if (string.IsNullOrWhiteSpace(refreshToken))
+            {
+                throw new ArgumentException(EmptyOrWhiteSpaceExceptionString, nameof(refreshToken));
+            }
+
+            MailService mailService = GetMailService(mailServiceName);
+            IRefreshable refresher = _authorizationProvider.CreateRefreshTokenClient(mailService);
+            AuthorizationToken freshToken = await refresher.RefreshTokenAsync(CreateToken(refreshToken), cancellationToken).ConfigureAwait(false);
+
+            return new AuthToken()
+            {
+                AccessToken = freshToken.AccessToken,
+                RefreshToken = freshToken.RefreshToken,
+                ExpiresIn = freshToken.ExpiresIn,
+            };
+        }
+
+        private static MailService GetMailService(string mailServiceName)
+        {
+            return Enum.TryParse(mailServiceName, out MailService mailService) && mailService != MailService.Unknown
+                ? mailService
+                : throw new AuthenticationException(EmailServiceNotSupportedExceptionString(mailServiceName));
+        }
+
+        private static AuthorizationToken CreateToken(string refreshToken)
+        {
+            return new AuthorizationToken(accessToken: string.Empty,
+                                          refreshToken: refreshToken,
+                                          tokenType: Token.BearerType,
+                                          expiresIn: TimeSpan.Zero,
+                                          scope: string.Empty);
+        }
+    }
+}
---
+++ src/Eppie.App/Tuvi.OAuth2/Toolkit.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Web;
+
+namespace Tuvi.OAuth2
+{
+    public static class Toolkit
+    {
+        public static NameValueCollection ParseQueryString(string uriString)
+        {
+            try
+            {
+                Uri uri = new UriBuilder(uriString).Uri;
+                return ParseQueryString(uri);
+            }
+            catch (UriFormatException)
+            {
+                return new NameValueCollection();
+            }
+        }
+
+        public static NameValueCollection ParseQueryString(Uri uri)
+        {
+            try
+            {
+                return HttpUtility.ParseQueryString(uri?.Query);
+            }
+            catch (ArgumentNullException)
+            {
+                return new NameValueCollection();
+            }
+        }
+    }
+}
