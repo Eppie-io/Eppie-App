0a8e78cff170756ceb1d5437222d0448e93b58fe
alexander <kauhan+github@eppie.io>
2024-08-29 12:54:50 +0300

    feat: add the UWP project to the solution
    References: #323

---
+++ source/Eppie.App/Eppie.App.UWP/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Eppie.App.UWP")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ToDo")]
+[assembly: AssemblyProduct("Eppie.App.UWP")]
+[assembly: AssemblyCopyright("Copyright © ToDo 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
