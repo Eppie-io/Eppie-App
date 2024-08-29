fcd5b355722e48b8b3a3fe40fd1031dbdeac906d
alexander <116556921+al-kau@users.noreply.github.com>
2024-08-29 13:39:34 +0300

    feat: create an Uno Platform app (#322)
    Use Uno.Sdk v5.3.99 and the Uno Platform App template with
    the following configuration:
    - Framework: .NET 8.0;
    - Platforms: Android, iOS, WebAssembly, macOS (Catalyst), Windows,
      Desktop;
    - Presentation: MVVM;
    - Markup: XAML;
    - Theme: Fluent, Theme Service;
    - Extensions: Dependency Injection, Configuration, Localization,
      Serilog;
    - Features: Toolkit;
    - Application: ID, Publisher;
    - Authentication: None;
    - Testing: None;
    - CI Pipeline: None;
    
    Resolves: #321

---
+++ source/Eppie.App/Eppie.App/GlobalUsings.cs
@@ -0,0 +1,10 @@
+﻿global using System.Collections.Immutable;
+global using CommunityToolkit.Mvvm.ComponentModel;
+global using CommunityToolkit.Mvvm.Input;
+global using Eppie.App.Models;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Localization;
+global using Microsoft.Extensions.Logging;
+global using Microsoft.Extensions.Options;
+global using ApplicationExecutionState = Windows.ApplicationModel.Activation.ApplicationExecutionState;
