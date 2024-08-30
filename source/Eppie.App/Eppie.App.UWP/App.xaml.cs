5d5689a7b9a81b847c8f48f811dc5d0c928715c9
alexander <116556921+al-kau@users.noreply.github.com>
2024-08-30 19:41:14 +0300

    feat: add the shared project (#329)
    * feat: add the shared project to the solution
    
    References: #326
    
    * build(deps): add packages
    
    Add nugets:
    - add Microsoft.UI.Xaml
    - add Uno.Toolkit.UI.
    
    References: #326
    
    * feat: move the xaml page into the shared project
    
    Move the files MainPage.xaml and MainPage.cs into Eppie.App.Shared
    project
    
    References: #326
    
    * feat: move resources into the shared project
    
    Move the files .resw into the Eppie.App.Shared project and set "en"
    as the default language.
    
    References: #326

--- source/Eppie.App/Eppie.App.UWP/App.xaml.cs
+++ source/Eppie.App/Eppie.App.UWP/App.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -66,7 +66,7 @@ namespace Eppie.App.UWP
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(Shared.Views.MainPage), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
