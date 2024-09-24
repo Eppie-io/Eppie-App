e8b62cad1b3c8093914d72aebb2e7f4604ff14be
alexander <kauhan+github@eppie.io>
2024-09-24 15:26:42 +0300

    build: fix file encoding
    References: Eppie-io/Eppie-App#332

--- source/Eppie.App/Eppie.App/GlobalUsings.cs
+++ source/Eppie.App/Eppie.App/GlobalUsings.cs
@@ -1,4 +1,4 @@
-﻿global using System.Collections.Immutable;
+global using System.Collections.Immutable;
 global using CommunityToolkit.Mvvm.ComponentModel;
 global using CommunityToolkit.Mvvm.Input;
 global using Eppie.App.Models;
