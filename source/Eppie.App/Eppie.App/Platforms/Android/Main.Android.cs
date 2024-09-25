b7abee073531c9780d21aebbefad19cc8667d937
alexander <116556921+al-kau@users.noreply.github.com>
2024-09-25 11:38:12 +0300

    refactor: systematize the namespace declaration  (#350)
    * build: enforce code style
    
    References: Eppie-io/Eppie-App#331
    
    * refactor: systematize the namespace declaration
    
    Define the rules 'csharp_style_namespace_declarations',
    IDE0160 and IDE0161.
    
    References: Eppie-io/Eppie-App#331

--- source/Eppie.App/Eppie.App/Platforms/Android/Main.Android.cs
+++ source/Eppie.App/Eppie.App/Platforms/Android/Main.Android.cs
@@ -11,33 +11,35 @@ using Android.Widget;
 using Com.Nostra13.Universalimageloader.Core;
 using Microsoft.UI.Xaml.Media;
 
-namespace Eppie.App.Droid;
-[global::Android.App.ApplicationAttribute(
-    Label = "@string/ApplicationName",
-    Icon = "@mipmap/icon",
-    LargeHeap = true,
-    HardwareAccelerated = true,
-    Theme = "@style/AppTheme"
-)]
-public class Application : Microsoft.UI.Xaml.NativeApplication
+namespace Eppie.App.Droid
 {
-
-    public Application(IntPtr javaReference, JniHandleOwnership transfer)
-        : base(() => new App(), javaReference, transfer)
+    [global::Android.App.ApplicationAttribute(
+        Label = "@string/ApplicationName",
+        Icon = "@mipmap/icon",
+        LargeHeap = true,
+        HardwareAccelerated = true,
+        Theme = "@style/AppTheme"
+    )]
+    public class Application : Microsoft.UI.Xaml.NativeApplication
     {
-        ConfigureUniversalImageLoader();
+
+        public Application(IntPtr javaReference, JniHandleOwnership transfer)
+            : base(() => new App(), javaReference, transfer)
+        {
+            ConfigureUniversalImageLoader();
+        }
+
+        private static void ConfigureUniversalImageLoader()
+        {
+            // Create global configuration and initialize ImageLoader with this config
+            ImageLoaderConfiguration config = new ImageLoaderConfiguration
+                .Builder(Context)
+                .Build();
+
+            ImageLoader.Instance.Init(config);
+
+            ImageSource.DefaultImageLoader = ImageLoader.Instance.LoadImageAsync;
+        }
     }
 
-    private static void ConfigureUniversalImageLoader()
-    {
-        // Create global configuration and initialize ImageLoader with this config
-        ImageLoaderConfiguration config = new ImageLoaderConfiguration
-            .Builder(Context)
-            .Build();
-
-        ImageLoader.Instance.Init(config);
-
-        ImageSource.DefaultImageLoader = ImageLoader.Instance.LoadImageAsync;
-    }
 }
-
