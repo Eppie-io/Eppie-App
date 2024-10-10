6b814c6a1fc0c1c3b7d0dc7a9183de21254be74c
alexander <116556921+al-kau@users.noreply.github.com>
2024-10-10 15:35:31 +0300

    chore: move Behaviors to Eppie.App.Shared (#373)
    * chore: move Behaviors to Eppie.App.Shared
    
    References: Eppie-io/Eppie-App#347
    
    * style: fix whitespace formatting
    
    References: Eppie-io/Eppie-App#347

--- source/Eppie.App/Eppie.App.Shared/Behaviors/ListViewItemClickBehavior.cs
+++ source/Eppie.App/Eppie.App.Shared/Behaviors/ListViewItemClickBehavior.cs
@@ -1,7 +1,13 @@
-﻿using Microsoft.Xaml.Interactivity;
+using Microsoft.Xaml.Interactivity;
 using System.Windows.Input;
+
+#if WINDOWS_UWP
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+#else 
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+#endif
 
 namespace Tuvi.App.Shared.Behaviors
 {
@@ -38,7 +44,7 @@ namespace Tuvi.App.Shared.Behaviors
             if (ClickCommand != null && ClickCommand.CanExecute(e?.ClickedItem))
             {
                 ClickCommand.Execute(e?.ClickedItem);
-            }            
+            }
         }
     }
 }
