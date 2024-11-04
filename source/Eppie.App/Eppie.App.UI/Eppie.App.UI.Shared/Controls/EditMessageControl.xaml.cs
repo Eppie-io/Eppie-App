293ce01ac57c7455e55bc0bfe7583ebb3b270ae0
alexander <kauhan+github@eppie.io>
2024-11-04 14:05:36 +0200

    feat: create EditMessageControl
    References: Eppie-io/Eppie-App#382

---
+++ source/Eppie.App/Eppie.App.UI/Eppie.App.UI.Shared/Controls/EditMessageControl.xaml.cs
@@ -0,0 +1,36 @@
+#if WINDOWS_UWP
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+#else
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+#endif
+
+namespace Eppie.App.UI.Controls
+{
+    public sealed partial class EditMessageControl : UserControl
+    {
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register(nameof(Text), typeof(string), typeof(MessageControl), new PropertyMetadata(null));
+
+        public string Html
+        {
+            get { return (string)GetValue(HtmlProperty); }
+            set { SetValue(HtmlProperty, value); }
+        }
+
+        public static readonly DependencyProperty HtmlProperty =
+            DependencyProperty.Register(nameof(Html), typeof(string), typeof(MessageControl), new PropertyMetadata(null));
+
+        public EditMessageControl()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
