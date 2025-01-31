df35ded988d71e50910ebb3ba64a6653e375fa8c
alexander <116556921+al-kau@users.noreply.github.com>
2025-01-31 16:32:00 +0200

    feat: add WebView2 to uno (#515)

---
+++ src/Eppie.App/Eppie.App.UI/Eppie.App.UI.Uno/Controls/MessageControl.xaml.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Threading.Tasks;
+using Tuvi.App.Shared.Controls;
+
+namespace Eppie.App.UI.Controls
+{
+    public sealed partial class MessageControl : BaseUserControl
+    {
+        private async Task UpdateHtmlView()
+        {
+            await HtmlView.EnsureCoreWebView2Async();
+
+            HtmlView.CoreWebView2.Settings.IsScriptEnabled = false;
+            HtmlView.NavigateToString(HtmlBody);
+        }
+    }
+}
