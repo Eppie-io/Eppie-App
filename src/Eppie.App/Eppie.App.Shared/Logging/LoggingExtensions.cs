e9fdfb548cb3c6fc60c3444c5c18dc02b9e43142
alexander <116556921+al-kau@users.noreply.github.com>
2025-05-31 20:22:29 +0300

    feat: add logging levels (#699)

---
+++ src/Eppie.App/Eppie.App.Shared/Logging/LoggingExtensions.cs
@@ -0,0 +1,68 @@
+﻿// ---------------------------------------------------------------------------- //
+//                                                                              //
+//   Copyright 2025 Eppie (https://eppie.io)                                    //
+//                                                                              //
+//   Licensed under the Apache License, Version 2.0 (the "License"),            //
+//   you may not use this file except in compliance with the License.           //
+//   You may obtain a copy of the License at                                    //
+//                                                                              //
+//       http://www.apache.org/licenses/LICENSE-2.0                             //
+//                                                                              //
+//   Unless required by applicable law or agreed to in writing, software        //
+//   distributed under the License is distributed on an "AS IS" BASIS,          //
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   //
+//   See the License for the specific language governing permissions and        //
+//   limitations under the License.                                             //
+//                                                                              //
+// ---------------------------------------------------------------------------- //
+
+using Microsoft.Extensions.Logging;
+using Serilog.Events;
+
+namespace Eppie.App.Shared.Logging
+{
+    public static class LoggingExtensions
+    {
+        public static LogEventLevel ToLogEventLevel(this LogLevel logLevel)
+        {
+            switch (logLevel)
+            {
+                case LogLevel.Trace:
+                    return LogEventLevel.Verbose;
+                case LogLevel.Debug:
+                    return LogEventLevel.Debug;
+                case LogLevel.Information:
+                    return LogEventLevel.Information;
+                case LogLevel.Warning:
+                    return LogEventLevel.Warning;
+                case LogLevel.Error:
+                    return LogEventLevel.Error;
+                case LogLevel.Critical:
+                    return LogEventLevel.Fatal;
+                default:
+                    return LevelAlias.Off;
+            }
+        }
+
+        public static LogLevel ToLogLevel(this LogEventLevel logEventLevel)
+        {
+            switch (logEventLevel)
+            {
+                case LogEventLevel.Verbose:
+                    return LogLevel.Trace;
+                case LogEventLevel.Debug:
+                    return LogLevel.Debug;
+                case LogEventLevel.Information:
+                    return LogLevel.Information;
+                case LogEventLevel.Warning:
+                    return LogLevel.Warning;
+                case LogEventLevel.Error:
+                    return LogLevel.Error;
+                case LogEventLevel.Fatal:
+                    return LogLevel.Critical;
+                default:
+                    return LogLevel.None;
+            }
+        }
+    }
+}
