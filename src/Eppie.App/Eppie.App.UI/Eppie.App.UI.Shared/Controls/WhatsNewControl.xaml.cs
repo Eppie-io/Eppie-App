521e2f287554af948450c504363206d9780560fb
Valera <valera@finebits.com>
2025-06-05 14:38:03 +0300

    feat: add what's new

---
+++ src/Eppie.App/Eppie.App.UI/Eppie.App.UI.Shared/Controls/WhatsNewControl.xaml.cs
@@ -0,0 +1,36 @@
+﻿// ---------------------------------------------------------------------------- //
+//                                                                              //
+//   Copyright 2025 Eppie (https://eppie.io)                                    //
+//                                                                              //
+//   Licensed under the Apache License, Version 2.0 (the "License"),            //
+//   you may not use this file except in compliance with the License.           //
+//   You may obtain a copy of the License at                                    //
+//                                                                              //
+//       http://www.apache.org/licenses/LICENSE-2.0                             //
+//                                                                              //
+//   Unless required by applicable law or agreed to in writing, software        //
+//   distributed under the License is distributed on an "AS IS" BASIS,          //
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   //
+//   See the License for the specific language governing permissions and        //
+//   limitations under the License.                                             //
+//                                                                              //
+// ---------------------------------------------------------------------------- //
+
+#if WINDOWS_UWP
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+#else
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+#endif
+
+namespace Eppie.App.UI.Controls
+{
+    public sealed partial class WhatsNewControl : UserControl
+    {
+        public WhatsNewControl()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
