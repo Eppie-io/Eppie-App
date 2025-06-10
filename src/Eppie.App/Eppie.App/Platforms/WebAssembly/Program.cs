58a7a6facdcc051740807d83066098c0194a28d9
alexander <116556921+al-kau@users.noreply.github.com>
2025-06-10 15:12:44 +0300

    build: update wasm (#723)

--- src/Eppie.App/Eppie.App/Platforms/WebAssembly/Program.cs
+++ src/Eppie.App/Eppie.App/Platforms/WebAssembly/Program.cs
@@ -16,17 +16,20 @@
 //                                                                              //
 // ---------------------------------------------------------------------------- //
 
+using Uno.UI.Hosting;
+
 namespace Eppie.App
 {
     public class Program
     {
-        private static Eppie.App.Shared.App _app;
-
-        public static int Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Microsoft.UI.Xaml.Application.Start(_ => _app = new Eppie.App.Shared.App());
+            var host = UnoPlatformHostBuilder.Create()
+                                             .App(() => new Eppie.App.Shared.App())
+                                             .UseWebAssembly()
+                                             .Build();
 
-            return 0;
+            await host.RunAsync();
         }
     }
 }
