446defa3bf06a12807fc4189ffc3e24a11b3a629
alexander <116556921+al-kau@users.noreply.github.com>
2025-09-24 17:06:48 +0300

    fea: add blank decentralized email settings pages (#865)

--- src/Eppie.App/Eppie.App.ViewModels/BaseViewModel.cs
+++ src/Eppie.App/Eppie.App.ViewModels/BaseViewModel.cs
@@ -234,7 +234,24 @@ namespace Tuvi.App.ViewModels
 
             if (account.Email.IsDecentralized)
             {
-                NavigationService?.Navigate(nameof(DecentralizedAddressSettingsPageViewModel), account);
+                if (account.Email.Network == NetworkType.Eppie)
+                {
+                    NavigationService?.Navigate(nameof(EppieAddressSettingsPageViewModel), account);
+                }
+                else if (account.Email.Network == NetworkType.Bitcoin)
+                {
+                    NavigationService?.Navigate(nameof(BitcoinAddressSettingsPageViewModel), account);
+                }
+                // Todo: Issue #861: Add opening Ethereum decentralized email address settings
+                // if (account.Email.Network == NetworkType.Ethereum)
+                // {
+                //    NavigationService?.Navigate(nameof(EthereumAddressSettingsPageViewModel), account);
+                // }
+                else
+                {
+                    NavigationService?.Navigate(nameof(DecentralizedAddressSettingsPageViewModel), account);
+                }
+
             }
             else if (Proton.Extensions.IsProton(account.Email))
             {
