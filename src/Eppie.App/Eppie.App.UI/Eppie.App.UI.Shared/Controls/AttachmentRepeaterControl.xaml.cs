fe3279a6b0438319b6be8cac727df560743e58bf
alexander <kauhan+github@eppie.io>
2026-02-13 18:59:23 +0200

    feat: add AttachmentRepeaterControl
    References:  #1088

---
+++ src/Eppie.App/Eppie.App.UI/Eppie.App.UI.Shared/Controls/AttachmentRepeaterControl.xaml.cs
@@ -0,0 +1,74 @@
+﻿// ---------------------------------------------------------------------------- //
+//                                                                              //
+//   Copyright 2026 Eppie (https://eppie.io)                                    //
+//                                                                              //
+//   Licensed under the Apache License, Version 2.0 (the "License"),            //
+//   you may not use this file except in compliance with the License.           //
+//   You may obtain a copy of the License at                                    //
+//                                                                              //
+//       http://www.apache.org/licenses/LICENSE-2.0                             //
+//                                                                              //
+//   Unless required by applicable law or agreed to in writing, software        //
+//   distributed under the License is distributed on an "AS IS" BASIS,          //
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   //
+//   See the License for the specific language governing permissions and        //
+//   limitations under the License.                                             //
+//                                                                              //
+// ---------------------------------------------------------------------------- //
+
+using System.Collections.Generic;
+using Tuvi.Core.Entities;
+using System.Diagnostics.CodeAnalysis;
+
+
+#if WINDOWS_UWP
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+#else
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+#endif
+
+namespace Eppie.App.UI.Controls
+{
+    [SuppressMessage("Design", "CA1010:Generic collections should implement generic interface", Justification = "ContentControl implements IEnumerable for XAML infrastructure")]
+    public sealed partial class AttachmentRepeaterControl : UserControl
+    {
+        public IList<Attachment> Items
+        {
+            get { return (IList<Attachment>)GetValue(ItemsProperty); }
+            set { SetValue(ItemsProperty, value); }
+        }
+        public static readonly DependencyProperty ItemsProperty =
+            DependencyProperty.Register(nameof(Items), typeof(IList<Attachment>), typeof(AttachmentRepeaterControl), new PropertyMetadata(null));
+
+        public DataTemplate ItemTemplate
+        {
+            get { return (DataTemplate)GetValue(ItemTemplateProperty); }
+            set { SetValue(ItemTemplateProperty, value); }
+        }
+        public static readonly DependencyProperty ItemTemplateProperty =
+            DependencyProperty.Register(nameof(ItemTemplate), typeof(DataTemplate), typeof(AttachmentRepeaterControl), new PropertyMetadata(null));
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register(nameof(Title), typeof(string), typeof(AttachmentRepeaterControl), new PropertyMetadata(null));
+
+        public bool IsTitleVisible
+        {
+            get { return (bool)GetValue(IsTitleVisibleProperty); }
+            set { SetValue(IsTitleVisibleProperty, value); }
+        }
+        public static readonly DependencyProperty IsTitleVisibleProperty =
+            DependencyProperty.Register(nameof(IsTitleVisible), typeof(bool), typeof(AttachmentRepeaterControl), new PropertyMetadata(false));
+
+        public AttachmentRepeaterControl()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ src/Eppie.App/Eppie.App.UI/Eppie.App.UI.Shared/Controls/ItemContainer.xaml.cs
@@ -0,0 +1,109 @@
+﻿// ---------------------------------------------------------------------------- //
+//                                                                              //
+//   Copyright 2026 Eppie (https://eppie.io)                                    //
+//                                                                              //
+//   Licensed under the Apache License, Version 2.0 (the "License"),            //
+//   you may not use this file except in compliance with the License.           //
+//   You may obtain a copy of the License at                                    //
+//                                                                              //
+//       http://www.apache.org/licenses/LICENSE-2.0                             //
+//                                                                              //
+//   Unless required by applicable law or agreed to in writing, software        //
+//   distributed under the License is distributed on an "AS IS" BASIS,          //
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   //
+//   See the License for the specific language governing permissions and        //
+//   limitations under the License.                                             //
+//                                                                              //
+// ---------------------------------------------------------------------------- //
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+#if WINDOWS_UWP
+using Windows.Devices.Input;
+using Windows.UI.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Markup;
+#else
+using Microsoft.UI.Input;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Markup;
+#endif
+
+namespace Eppie.App.UI.Controls
+{
+    [SuppressMessage("Design", "CA1010:Generic collections should implement generic interface", Justification = "ContentControl implements IEnumerable for XAML infrastructure")]
+    [ContentProperty(Name = nameof(ItemContent))]
+    public sealed partial class ItemContainer : UserControl
+    {
+        public event EventHandler Invoked;
+
+        public UIElement ItemContent
+        {
+            get { return (UIElement)GetValue(ItemContentProperty); }
+            set { SetValue(ItemContentProperty, value); }
+        }
+
+        public static readonly DependencyProperty ItemContentProperty =
+            DependencyProperty.Register(nameof(ItemContent), typeof(UIElement), typeof(ItemContainer), new PropertyMetadata(null));
+
+
+        private bool _canInvoke;
+        private bool _isPointerOver;
+
+        public ItemContainer()
+        {
+            this.InitializeComponent();
+
+            InitializeVisualStateManager();
+        }
+
+        private void InitializeVisualStateManager()
+        {
+            RootGrid.PointerExited += (s, e) =>
+            {
+                _canInvoke = false;
+                _isPointerOver = false;
+                VisualStateManager.GoToState(this, "Normal", true);
+            };
+
+            RootGrid.PointerEntered += (s, e) =>
+            {
+                _canInvoke = false;
+                _isPointerOver = true;
+                VisualStateManager.GoToState(this, "PointerOver", true);
+            };
+
+            RootGrid.PointerReleased += (s, e) => VisualStateManager.GoToState(this, _isPointerOver ? "PointerOver" : "Normal", true);
+
+            RootGrid.PointerPressed += (s, e) =>
+            {
+                if (s is UIElement element)
+                {
+                    PointerPointProperties properties = e.GetCurrentPoint(element)?.Properties;
+
+                    _canInvoke = e.Pointer.PointerDeviceType != PointerDeviceType.Mouse || properties?.IsLeftButtonPressed == true;
+
+                    if (_canInvoke && Invoked != null)
+                    {
+                        VisualStateManager.GoToState(this, "Pressed", true);
+                    }
+                }
+            };
+        }
+
+        private void OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (_canInvoke)
+            {
+                Invoked?.Invoke(this, EventArgs.Empty);
+            }
+
+            _canInvoke = false;
+        }
+    }
+}
