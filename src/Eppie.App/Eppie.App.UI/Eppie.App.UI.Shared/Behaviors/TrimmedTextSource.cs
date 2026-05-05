cd31bb30be6219c8a41594538d6dfad2c078ca32
alexander <kauhan+github@eppie.io>
2026-05-05 17:10:41 +0300

    feat: add TrimmedTextTooltipBehavior
    References:  #1088

---
+++ src/Eppie.App/Eppie.App.UI/Eppie.App.UI.Shared/Behaviors/TrimmedTextSource.cs
@@ -0,0 +1,69 @@
+﻿// ---------------------------------------------------------------------------- //
+//                                                                              //
+//   Copyright 2026 Eppie (https://eppie.io)                                    //
+//                                                                              //
+//   Licensed under the Apache License, Version 2.0 (the "License"),            //
+//   you may not use this file except in compliance with the License.           //
+//   You may obtain a copy of the License at                                    //
+//                                                                              //
+//       http://www.apache.org/licenses/LICENSE-2.0                             //
+//                                                                              //
+//   Unless required by applicable law or agreed to in writing, software        //
+//   distributed under the License is distributed on an "AS IS" BASIS,          //
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   //
+//   See the License for the specific language governing permissions and        //
+//   limitations under the License.                                             //
+//                                                                              //
+// ---------------------------------------------------------------------------- //
+
+using System;
+
+#if WINDOWS_UWP
+using Windows.UI.Xaml.Controls;
+#else 
+using Microsoft.UI.Xaml.Controls;
+#endif
+
+namespace Eppie.App.UI.Behaviors
+{
+    public interface ITrimmedTextSource
+    {
+        bool IsTextTrimmed { get; }
+        string Text { get; }
+
+        event EventHandler<IsTextTrimmedChangedEventArgs> IsTextTrimmedChanged;
+    }
+
+    public class TrimmedTextBlockSource : ITrimmedTextSource
+    {
+        public bool IsTextTrimmed => Source?.IsTextTrimmed ?? false;
+        public string Text => Source?.Text;
+
+        public event EventHandler<IsTextTrimmedChangedEventArgs> IsTextTrimmedChanged;
+
+        private TextBlock _source;
+        public TextBlock Source
+        {
+            get { return _source; }
+            set
+            {
+                if (_source != null)
+                {
+                    _source.IsTextTrimmedChanged -= OnIsTextTrimmedChanged;
+                }
+
+                _source = value;
+
+                if (_source != null)
+                {
+                    _source.IsTextTrimmedChanged += OnIsTextTrimmedChanged;
+                }
+            }
+        }
+
+        private void OnIsTextTrimmedChanged(TextBlock sender, IsTextTrimmedChangedEventArgs args)
+        {
+            IsTextTrimmedChanged?.Invoke(this, args);
+        }
+    }
+}
---
+++ src/Eppie.App/Eppie.App.UI/Eppie.App.UI.Shared/Behaviors/TrimmedTextTooltipBehavior.cs
@@ -0,0 +1,123 @@
+﻿// ---------------------------------------------------------------------------- //
+//                                                                              //
+//   Copyright 2026 Eppie (https://eppie.io)                                    //
+//                                                                              //
+//   Licensed under the Apache License, Version 2.0 (the "License"),            //
+//   you may not use this file except in compliance with the License.           //
+//   You may obtain a copy of the License at                                    //
+//                                                                              //
+//       http://www.apache.org/licenses/LICENSE-2.0                             //
+//                                                                              //
+//   Unless required by applicable law or agreed to in writing, software        //
+//   distributed under the License is distributed on an "AS IS" BASIS,          //
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   //
+//   See the License for the specific language governing permissions and        //
+//   limitations under the License.                                             //
+//                                                                              //
+// ---------------------------------------------------------------------------- //
+
+using System;
+using Microsoft.Xaml.Interactivity;
+
+#if WINDOWS_UWP
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+#else 
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+#endif
+
+namespace Eppie.App.UI.Behaviors
+{
+    public class TrimmedTextTooltipBehavior : Behavior<DependencyObject>
+    {
+        public ITrimmedTextSource Source
+        {
+            get { return (ITrimmedTextSource)GetValue(SourceProperty); }
+            set { SetValue(SourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty SourceProperty =
+            DependencyProperty.Register(nameof(Source), typeof(ITrimmedTextSource), typeof(TrimmedTextTooltipBehavior), new PropertyMetadata(null, OnPropertyChanged));
+
+        private static void OnPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is TrimmedTextTooltipBehavior behavior)
+            {
+                if (e.OldValue is ITrimmedTextSource oldSource)
+                {
+                    oldSource.IsTextTrimmedChanged -= behavior.OnSourceIsTextTrimmedChanged;
+                }
+
+                if (e.NewValue is ITrimmedTextSource newSource)
+                {
+                    newSource.IsTextTrimmedChanged += behavior.OnSourceIsTextTrimmedChanged;
+                }
+            }
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            if (Source != null)
+            {
+                Source.IsTextTrimmedChanged += OnSourceIsTextTrimmedChanged;
+            }
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            if (Source != null)
+            {
+                Source.IsTextTrimmedChanged -= OnSourceIsTextTrimmedChanged;
+            }
+        }
+
+        private void OnSourceIsTextTrimmedChanged(object sender, IsTextTrimmedChangedEventArgs args)
+        {
+            UpdateToolTip();
+        }
+
+        private void UpdateToolTip()
+        {
+            string tooltip = Source.Text;
+
+            if (!Source.IsTextTrimmed || string.IsNullOrEmpty(tooltip))
+            {
+                ToolTipService.SetToolTip(AssociatedObject, null);
+            }
+            else
+            {
+                ToolTipService.SetToolTip(AssociatedObject, tooltip);
+            }
+        }
+    }
+
+    public class TrimmedTextBlockTooltipBehavior : Behavior<TextBlock>
+    {
+        private TrimmedTextTooltipBehavior _behavior;
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            _behavior = new TrimmedTextTooltipBehavior()
+            {
+                Source = new TrimmedTextBlockSource() { Source = AssociatedObject }
+            };
+
+            _behavior.Attach(AssociatedObject);
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            _behavior.Detach();
+            _behavior = null;
+        }
+    }
+}
