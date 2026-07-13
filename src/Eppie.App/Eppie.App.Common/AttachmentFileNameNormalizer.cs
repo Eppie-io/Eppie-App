9ac1831f03d76030b6e6a50744f5097f02bd44af
Valera <50830352+ValeraFinebits@users.noreply.github.com>
2026-07-13 22:09:19 +0300

    fix: add attachment filename normalization (#1257)

---
+++ src/Eppie.App/Eppie.App.Common/AttachmentFileNameNormalizer.cs
@@ -0,0 +1,89 @@
+﻿// ---------------------------------------------------------------------------- //
+//                                                                              //
+//   Copyright 2026 Eppie (https://eppie.io)                                    //
+//                                                                              //
+//   Licensed under the Apache License, Version 2.0 (the "License"),            //
+//   you may not use this file except in compliance with the License.           //
+//   You may obtain a copy of the License at                                    //
+//                                                                              //
+//       http://www.apache.org/licenses/LICENSE-2.0                             //
+//                                                                              //
+//   Unless required by applicable law or agreed to in writing, software        //
+//   distributed under the License is distributed on an "AS IS" BASIS,          //
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   //
+//   See the License for the specific language governing permissions and        //
+//   limitations under the License.                                             //
+//                                                                              //
+// ---------------------------------------------------------------------------- //
+
+using System;
+using System.IO;
+
+namespace Eppie.App.Common
+{
+    public static class AttachmentFileNameNormalizer
+    {
+        /// <summary>
+        /// Normalizes attachment file name for safe file system usage while preserving the extension format.
+        /// </summary>
+        /// <param name="fileName">Original attachment file name.</param>
+        /// <returns>Normalized file name.</returns>
+        public static string Normalize(string fileName)
+        {
+            const string defaultFileName = "attachment";
+
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                return defaultFileName;
+            }
+
+            var fileNameOnly = Path.GetFileName(fileName);
+            var extension = NormalizeExtension(Path.GetExtension(fileNameOnly));
+            var fileNameWithoutExtension = NormalizeFileNamePart(Path.GetFileNameWithoutExtension(fileNameOnly));
+
+            if (string.IsNullOrWhiteSpace(fileNameWithoutExtension))
+            {
+                fileNameWithoutExtension = defaultFileName;
+            }
+
+            return string.IsNullOrWhiteSpace(extension)
+                ? fileNameWithoutExtension
+                : fileNameWithoutExtension + extension;
+        }
+
+        private static string NormalizeExtension(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return string.Empty;
+            }
+
+            var normalizedExtension = NormalizeFileNamePart(value.TrimStart('.'));
+
+            return string.IsNullOrWhiteSpace(normalizedExtension)
+                ? string.Empty
+                : "." + normalizedExtension;
+        }
+
+        private static string NormalizeFileNamePart(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return string.Empty;
+            }
+
+            var invalidFileNameChars = Path.GetInvalidFileNameChars();
+            var normalizedFileNameChars = value.ToCharArray();
+
+            for (int i = 0; i < normalizedFileNameChars.Length; i++)
+            {
+                if (Array.IndexOf(invalidFileNameChars, normalizedFileNameChars[i]) >= 0)
+                {
+                    normalizedFileNameChars[i] = '_';
+                }
+            }
+
+            return new string(normalizedFileNameChars).Trim(' ', '.');
+        }
+    }
+}
---
+++ src/Eppie.App/Tests/Eppie.App.UI.Tests/AttachmentFileNameNormalizerTests.cs
@@ -0,0 +1,106 @@
+﻿// ---------------------------------------------------------------------------- //
+//                                                                              //
+//   Copyright 2026 Eppie (https://eppie.io)                                    //
+//                                                                              //
+//   Licensed under the Apache License, Version 2.0 (the "License"),            //
+//   you may not use this file except in compliance with the License.           //
+//   You may obtain a copy of the License at                                    //
+//                                                                              //
+//       http://www.apache.org/licenses/LICENSE-2.0                             //
+//                                                                              //
+//   Unless required by applicable law or agreed to in writing, software        //
+//   distributed under the License is distributed on an "AS IS" BASIS,          //
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   //
+//   See the License for the specific language governing permissions and        //
+//   limitations under the License.                                             //
+//                                                                              //
+// ---------------------------------------------------------------------------- //
+
+using Eppie.App.Common;
+using NUnit.Framework;
+
+namespace Eppie.App.UI.Tests.Services
+{
+    [TestFixture]
+    public class AttachmentFileNameNormalizerTests
+    {
+        [TestCase(null, "attachment")]
+        [TestCase("", "attachment")]
+        [TestCase(" ", "attachment")]
+        [TestCase("   ", "attachment")]
+        [TestCase("\t", "attachment")]
+        [TestCase("\r\n", "attachment")]
+        [TestCase("report", "report")]
+        [TestCase("report.pdf", "report.pdf")]
+        [TestCase("archive.tar.gz", "archive.tar.gz")]
+        [TestCase("file.", "file")]
+        [TestCase("file..", "file")]
+        [TestCase(" report .pdf ", "report.pdf")]
+        public void NormalizeReturnsExpectedValueForGeneralInputs(string? fileName, string expected)
+        {
+            var normalized = AttachmentFileNameNormalizer.Normalize(fileName!);
+
+            Assert.That(normalized, Is.EqualTo(expected));
+        }
+
+        [TestCase("folder\\report.pdf", "report.pdf")]
+        [TestCase("folder\\nested\\report.pdf", "report.pdf")]
+        [TestCase("C:\\temp\\report.pdf", "report.pdf")]
+        [TestCase("folder/report.pdf", "report.pdf")]
+        [TestCase("folder/sub/report.pdf", "report.pdf")]
+        [TestCase("./report.pdf", "report.pdf")]
+        [TestCase("../report.pdf", "report.pdf")]
+        [TestCase("folder\\report", "report")]
+        [TestCase("folder/sub/archive.tar.gz", "archive.tar.gz")]
+        [TestCase("C:/Temp Folder/monthly report.pdf", "monthly report.pdf")]
+        public void NormalizeWhenPathProvidedReturnsFileNameOnly(string fileName, string expected)
+        {
+            var normalized = AttachmentFileNameNormalizer.Normalize(fileName);
+
+            Assert.That(normalized, Is.EqualTo(expected));
+        }
+
+        [TestCase(".txt", "attachment.txt")]
+        [TestCase(".pdf", "attachment.pdf")]
+        [TestCase(".json", "attachment.json")]
+        [TestCase(".xml", "attachment.xml")]
+        [TestCase(".zip", "attachment.zip")]
+        [TestCase(".gz", "attachment.gz")]
+        [TestCase(".png", "attachment.png")]
+        [TestCase(".jpg", "attachment.jpg")]
+        [TestCase(".docx", "attachment.docx")]
+        [TestCase(".tar", "attachment.tar")]
+        public void NormalizeWhenBaseNameMissingPreservesExtensionWithDefaultName(string fileName, string expected)
+        {
+            var normalized = AttachmentFileNameNormalizer.Normalize(fileName);
+
+            Assert.That(normalized, Is.EqualTo(expected));
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        [TestCase(6)]
+        [TestCase(7)]
+        [TestCase(8)]
+        [TestCase(9)]
+        public void NormalizeWhenInvalidCharactersPresentReplacesThemAndPreservesExtension(int invalidCharIndex)
+        {
+            var invalidFileNameChar = InvalidFileNameTestChars[invalidCharIndex % InvalidFileNameTestChars.Length];
+            var fileName = $"re{invalidFileNameChar}port.p{invalidFileNameChar}df";
+
+            var normalized = AttachmentFileNameNormalizer.Normalize(fileName);
+
+            Assert.That(normalized, Is.EqualTo("re_port.p_df"));
+        }
+
+        private static readonly char[] InvalidFileNameTestChars = Path.GetInvalidFileNameChars()
+            .Where(ch => ch != '.' && ch != Path.DirectorySeparatorChar && ch != Path.AltDirectorySeparatorChar)
+            .Distinct()
+            .DefaultIfEmpty('\0')
+            .ToArray();
+    }
+}
